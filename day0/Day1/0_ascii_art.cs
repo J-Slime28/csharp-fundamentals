91fe61421478c09292c0cb70087aa54f708a20ce
Karabo Manala <karabowaynez28@gmail.com>
2026-01-21 21:52:45 +0200

    Delete day0/Day1 directory

--- day0/Day1/0_ascii_art.cs
+++
@@ -1,8 +0,0 @@
-using System;
-class Program{
-    static void PrintAsciiArt(){
-        int n=int.Parse(Console.ReadLine());
-        for(int i=1;i<=n;i++){for(int j=0;j<i;j++){Console.Write("*");}Console.WriteLine();}
-    }
-    static void Main(){PrintAsciiArt();}
-}
